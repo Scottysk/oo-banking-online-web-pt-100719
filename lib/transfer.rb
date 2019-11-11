676a6ed86ad19367faa462e167daa2cd02e6297c
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-11-11 01:57:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
       @receiver.balance += self.amount
       @status = "complete"
     else
-      self.status = "rejected"
+      @status = "rejected"
       "Transaction rejected. Please check your account balanced."
     end
   end
