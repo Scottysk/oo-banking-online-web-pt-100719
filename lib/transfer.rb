ad597d892c4bcfd5af2d807a5e87a654547f19f6
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-11-11 01:56:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
   def execute_transaction
     if @sender.balance > @amount && @status == "pending"
       @sender.balance -= self.amount
-      @receiver.balance(self.amount)
+      @receiver.balance += self.amount)
       self.status = "reversed"
     else
       self.status = "rejected"
